72385cc677946fe13ec46883a9132ec48f3fe5a0
Eren Göktepe <127156306+physicci@users.noreply.github.com>
2023-08-24 22:42:21 +0300

    Update challange_9.py

--- challange_9.py
+++ challange_9.py
@@ -1,19 +1,21 @@
-# Bir diziye bağlı kalmadan, kullanıcın girdiği değerlerin içinden; çift-tek sayıların toplamını bulabileceğimiz bir kod parçası olusturdum.
+# Kullanıcıdan değerleri alarak toplamları hesaplayan kod
 sayilar = []
 n = int(input("Kaç tane sayi kullanacaksiniz?: "))
 for i in range(n):
-      sayi = int(input(f"{i+1}. sayiyi giriniz: "))
-      sayilar.append(sayi)
+    sayi = int(input(f"{i+1}. sayiyi giriniz: "))
+    sayilar.append(sayi)
 
 # Değişken kullanıyorum
 toplam_cift = 0
 toplam_tek = 0
+
 # 'for' döngüsü ile sayıları kontrol ediyoruz...
 for sayi in sayilar:
     if sayi % 2 == 0:  
-        toplam_cift += sayi  
-if sayi % 1 == 0:
-     toplam_tek += sayi
-# Sonuç;
+        toplam_cift += sayi
+    else:
+        toplam_tek += sayi
+
+# Sonuçları yazdırın
 print("Çift sayilarin toplami:", toplam_cift)
 print("Tek sayilarin toplami:", toplam_tek)
